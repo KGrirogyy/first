9fe5fc18d72aa9572ae84c9df460a73a2b677d03
KGrirogyy <kachurovskyy123@gmail.com>
2022-10-17 22:52:36 +0300

    #

--- darksite/js/script.js
+++ darksite/js/script.js
@@ -2,4 +2,5 @@ $('.burger').click(function(){
 	$(this).toggleClass('open');
 	$('.navigation').toggleClass('active')
 	$('.footer-form').toggleClass('hidden')
+	$('body').toggleClass('scroll-off')
 })
